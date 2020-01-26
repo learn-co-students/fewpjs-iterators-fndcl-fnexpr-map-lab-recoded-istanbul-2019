034faee9d5b0ee9437939b4f3b4f9b82d54b4476
Kaan <kkureli@gmail.com>
2020-01-27 00:39:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Contutorialuctor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+    'what does the this keyword mean?',
+    'What is the Contutorialuctor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
 ];
 
 const titleCased = () => {
-  return tutorials
+    return tutorials.map(titleCased => {
+        let empArr = []
+        let array1 = titleCased.split(" ")
+        array1.forEach(element => {
+            empArr.push(element[0].toUpperCase() + element.slice(1))
+        })
+        return empArr.join(" ")
+
+    })
 }
+
+
+
+
+// const titleCased = function titleCased(tutName) {
+//     let empArr = []
+//     let array1 = tutName.split(" ")
+
+//     array1.forEach(element => {
+//         empArr.push(element[0].toUpperCase() + element.slice(1))
+//     })
+//     console.log(empArr);
+
+//     return empArr.join(" ")
+// }
+
+
+// let newArray = tutorials.map(titleCased)

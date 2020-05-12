043487951e72fe5416bc61ff8615508a1c9d5b15
Dara <dara.dandashi@gmail.com>
2020-05-12 13:46:48 +0300

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,59 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  
+   let result = tutorials.map((line)=>{
+   let word= line.split(" ");
+   let letter = word.map((e)=>{
+   return e.charAt(0).toUpperCase() + e.slice(1);
+   })
+   let last= letter.join(" ")
+    return last
+  })
+  console.log(result)
+  return result;
 }
+titleCased(tutorials);
+// already writtin
+
+//1 map
+//2 split by " " 
+//3 uppercase
+//4 join
+
+
+
+
+
+// let words = tutorials.split(' ')
+
+// const titleCased = tutorials.map(words => tutorials.split(' ')) => {
+
+//   for (let i = 0 ; i< words.length ; i++){
+//     words[i] = words.charAt(0).toUpperCase()+ words[i].slice(1);
+//   }
+//   return words.join(' ');
+//   let cabsOn = words.map(fuction (titleCased){
+
+//   )}
+
+
+//   return tutorials // already writtin
+// }
+
+// function cabsOn(){
+//   let words = tutorials.split(' ');
+  
+// }
+
+
+// titleize(string) {
+//   let words = string.split(' ');
+//   for (let n = 0; n < words.length; n++) {
+//     words[n] = words[n].charAt(0).toUpperCase() + words[n].slice(1);
+//   }
+//   return words.join(' ');
+
+
+// static capitalize(string){
+//   return string.charAt(0).toUpperCase() + string.slice(1);
